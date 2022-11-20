107308e2cb3a2b7787622a5530b442554cd729ff
unknown <pshgesa@gmail.com>
2022-11-20 17:41:52 +0900

    winston 프로젝트에 적용

--- app/app.js
+++ app/app.js
@@ -4,11 +4,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-const morgan = require("morgan");
-const app = express();
 dotenv.config();
 
-const accessLogStream = require("./src/config/log");
+const app = express();
+
 // 라우팅
 const home = require("./src/routes/home");
 
@@ -17,8 +16,6 @@ app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extends: true }));
-app.use(morgan("dev"));
-app.use(morgan("tiny", { stream: accessLogStream }));
 
 app.use("/", home);
 
